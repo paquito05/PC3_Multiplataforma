a120c977741d2755f1897b55a9a16312ea8e2f8a
@ paquito05 <you@example.com>
2021-11-02 12:50:09 -0500

    Pregunta07 navegation PC-3

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-/* import 'package:componentes/src/pages/alert_page.dart';
-import 'package:componentes/src/pages/avatar_page.dart'; */
+import 'package:componentes/src/pages/alert_page.dart';
+//import 'package:componentes/src/pages/avatar_page.dart';
 import 'package:componentes/src/pages/home_page.dart';
 import 'package:flutter/material.dart';
 //import 'dart:ffi';
@@ -7,18 +7,17 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-    @override
-    Widget build(BuildContext context){
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
-        title :'Material App',
+        title: 'Material App',
         debugShowCheckedModeBanner: false,
         //home: Homepage(),
         initialRoute: '/',
-        routes:{
-        '/': (context) => HomePage()
-        /* 'alert': (context) -> AlertPage(),
-        'avatar': (context) => AvatarPage(), */
-            }
-        );
-    }
+        routes: {
+          '/': (context) => HomePage(),
+          'alert': (context) => ALertPage(),
+          //'avatar': (context) => AvatarPage(),
+        });
+  }
 }
